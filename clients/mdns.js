a3551fa8199b2878379f545eb2842adcdb7a2604
Andrew Nicolaou <andrew.nicolaou@bbc.co.uk>
2014-06-26 17:12:24 +0100

    Allow browsing of all mDNS services if no type specified

--- clients/mdns.js
+++ clients/mdns.js
@@ -1,16 +1,59 @@
 var mdns = require('mdns'),
     EventEmitter = require('events').EventEmitter,
+    browsers = {};
     serviceType = mdns.makeServiceType({name: 'http', protocol: 'tcp'});
 
-module.exports.create = function (urn) {
+module.exports.create = function (service) {
   var instance = new EventEmitter(),
-      urn      = urn ? urn : serviceType,
-      browser  = urn ? mdns.createBrowser(urn) : mdns.browseThemAll(),
-      started  = false;
+      browser,
+      started;
 
-  browser.on('serviceUp', function(msg) {
-    // console.log(msg);
+  // If browser exists for service
+  if ( service && browsers[service] ) {
+    browsers[service].on('serviceUp', createServiceUpHandler(instance));
+    browsers[service].on('serviceDown', createServiceDownHandler(instance));
+  // If no browser for service
+  } else if ( service ) {
+    browsers[service] = createBrowserWithHandlers(service, instance);
+  // Browse everything 
+  } else {
+    service = '*';
+    browsers[service] = mdns.browseThemAll();
+    browsers[service].on('serviceUp', function (msg) {
+      var service = mdns.makeServiceType(
+        { name: msg.type.name, protocol: msg.type.protocol }
+      );
+      browsers[service] = createBrowserWithHandlers(service, instance);
+      if (started) {
+        browsers[service].start();
+        browsers[service].started = true;
+      }
+    });
+  }
 
+  instance.start = function () {
+    Object.keys(browsers).forEach(function (service) {
+      if(!browsers[service].started) {
+        browsers[service].start();
+        browsers[service].started = true; 
+      }
+    });
+    started = true;
+  }
+
+  return instance;
+}
+
+function createBrowserWithHandlers(service, instance) {
+    var type = mdns.makeServiceType(service);
+    var browser = mdns.createBrowser(type);
+    browser.on('serviceUp', createServiceUpHandler(instance));
+    browser.on('serviceDown', createServiceDownHandler(instance));
+    return browser;
+}
+
+function createServiceUpHandler(instance) {
+  return function serviceUp(msg) {
     var output = {
       id: msg.fullname,
       type: msg.type.toString(),
@@ -24,11 +67,11 @@ module.exports.create = function (urn) {
     if(output.id) {
       instance.emit('*', output);
     }
-  });
+  }
+}
 
-  browser.on('serviceDown', function(msg) {
-    // console.log(msg);
-
+function createServiceDownHandler(instance) {
+  return function serviceDown(msg) {
     var output = {
       id: msg.name + '.' + msg.type.toString() + msg.replyDomain,
       type: msg.type.toString(),
@@ -42,14 +85,5 @@ module.exports.create = function (urn) {
     if(output.id) {
       instance.emit('*', output);
     }
-  });
-  
-  instance.start = function () {
-    if(!started) {
-     browser.start();
-     started = true; 
-    }
   }
-  
-  return instance;
 }
